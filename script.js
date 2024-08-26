f65885acb8f9f3c92e58cf9cf2c2fc06a3dc2954
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-26 23:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	return arr.reduce((res, curr) => {
+		const lChunk = res[res.length-1];
+		if(!lChunk || lChunk.reduce((sum, item) => sum + item, 0) + curr > n){
+			res.push([curr]);
+		}else {
+			lChunk.push(curr);
+		}
+		return res;
+	}, [])
 };
 
 const n = prompt("Enter n: ");
